44229f0e03ffd9d60d467b4fe34be238bdaaa151
Ben Straatsma <benstraatsma@gmail.com>
2018-03-24 12:13:30 -0400

    Fixed delete hook to only allow to delete friends of the current user

--- src/hooks/friend-deletion-restriction.js
+++ src/hooks/friend-deletion-restriction.js
@@ -5,9 +5,15 @@
 module.exports = function (options = {}) {
   return async context => {
     const id = context.id
+    const currUser = context.params.user
 
     if (id === null) {
       //make sure params are safe here and we dont delete a good request
+      context.params.query.$or= [{
+        user1: currUser
+      }, {
+        user2: currUser
+      }]
     } else {
       await context.service.get(id)
     }
