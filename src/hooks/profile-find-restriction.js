800190b4b3ab1f563da961493428f65a49909d76
Ben Straatsma <benstraatsma@gmail.com>
2018-03-29 12:13:08 -0400

    Allowed find to handle getting own profile, getting specific users profile, or just getting some friends profiles

--- src/hooks/profile-find-restriction.js
+++ src/hooks/profile-find-restriction.js
@@ -4,30 +4,70 @@
 // eslint-disable-next-line no-unused-vars
 module.exports = function (options = {}) {
   return async context => {
-    const userID = context.params.query.userID;
 
     if (context.params.user !== undefined && context.params.user !== null) {
       const currUser = "" + context.params.user._id
-      
-      await context.app.service('friends').find({
-        query: {
-          $or: [{
-            user1: userID,
-            user2: currUser
-          }, {
-            user1: currUser,
-            user2: userID
-          }]
-        }
-      }).then((data) => {
-        if (data.data.length) {
-          throw new Error('Users are not friends.');
-        }
-      })
-      
-    }
 
-    
+      const userID = context.params.query.userID
+      
+      if (userID !== undefined && userID !== null) {
+        if (userID !== currID) {
+          await context.app.service('friends').find({
+            query: {
+              $or: [{
+                user1: userID,
+                user2: currUser
+              }, {
+                user1: currUser,
+                user2: userID
+              }]
+            }
+          }).then((data) => {
+            if (data.data.length) {
+              throw new Error('Users are not friends.');
+            }
+          })
+        }
+      }
+
+      else {
+        const limit = context.params.query.$limit || 50
+        const skip = context.params.query.$skip || 0
+
+        //get all profiles of the friends of the user
+        await context.app.service('friends').find({
+          query: {
+            $or: [{
+              user1: currUser
+            }, {
+              user2: currUser
+            }],
+            $limit: limit,
+            $skip = skip
+          }
+        }).then((data) => {
+          //go over all returned users
+          const length = data.data.length
+          let users = []
+          let userIter = 0
+
+          for (let i=0; i<length; i++) {
+            if (data.data[i].user1 !== currUser && !users.includes(data.data[i].user1)) {
+              users[userIter] = objectid(users[userIter].user1)
+              userIter++
+            }
+            else if (data.data[i].user2 !== currUser && !users.includes(data.data[i].user2)) {
+              users[userIter] = objectid(users[userIter].user)
+              userIter++
+            }
+          }
+
+          context.params.query.userID = {
+            $in: users
+          }
+        })
+      }
+    }
 
     return context;
   };
