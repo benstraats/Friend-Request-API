bc952a6c48ff3b327dce6ebb43684a8979372e4b
Ben Straatsma <benstraatsma@gmail.com>
2018-03-29 12:47:30 -0400

    Added basic profile creation checking

--- src/hooks/profile-create-validation.js
+++ src/hooks/profile-create-validation.js
@@ -4,7 +4,51 @@
 // eslint-disable-next-line no-unused-vars
 module.exports = function (options = {}) {
   return async context => {
-    //TODO: add this
+    const currUser = "" + context.params.user._id
+
+    await context.service.find({
+      query: {
+        userID: currUser
+      }
+    }).then((data) => {
+      if (data.data.length) {
+        throw new Error('Profile for user has already been created')
+      }
+    })
+
+    /*
+    Must be in this format:
+    userID: someObjectID,
+    profile: [
+      {row:0, key:socialMediaPlatform, value:userHandle},
+      {row:1, key:socialMediaPlatform, value:userHandle},
+      {row:2, key:socialMediaPlatform, value:userHandle},
+      {row:3, key:socialMediaPlatform, value:userHandle},
+      {row:4, key:socialMediaPlatform, value:userHandle},
+      ...
+    ]
+    */
+    let curatedProfile = context.data.profile
+    for (let i=0; i<context.data.profile.length; i++) {
+      if (profile[i].row !== i) {
+        throw new Error('Rows are out of order');
+      }
+      if (!profile[i].key) {
+        throw new Error('Row must have a key');
+      }
+      if (!profile[i].value) {
+        throw new Error('Row must have a value');
+      }
+      let rowSave = profile[i].row
+      let keySave = profile[i].key
+      let valueSave = profile[i].value
+      curatedProfile[i] = {row:rowSave, key:keySave, value:valueSave}
+    }
+    
+    context.data = {
+      userID: currUser,
+      profile: curateProfile
+    }
 
     return context;
   };
