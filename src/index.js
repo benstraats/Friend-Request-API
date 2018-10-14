0ee70d3bd52fa9b4cf18a28629ff9784a770b729
Ben Straatsma <benstraatsma@gmail.com>
2018-10-14 12:21:12 -0400

    Bumped version to 0.9. Started using https. Removed sockets and channels. Updated some housecleaning

--- src/index.js
+++ src/index.js
@@ -2,12 +2,35 @@
 const logger = require('winston');
 const app = require('./app');
 const port = app.get('port');
-const server = app.listen(port);
 
-process.on('unhandledRejection', (reason, p) =>
-  logger.error('Unhandled Rejection at: Promise ', p, reason)
-);
+var http = require('http');
+http.createServer(function (req, res) {
+    res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
+    res.end();
+}).listen(80);
 
-server.on('listening', () =>
-  logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
+// HTTPS SUPPORT
+const https = require('https');
+const fs = require('fs');
+
+let authData = {
+// use your own keys
+  key: fs.readFileSync('/path/to/private.key'),
+  cert: fs.readFileSync('/path/to/certificate.crt'),
+  ca: [
+        fs.readFileSync('/path/to/intermediateCertOne.crt'),
+        fs.readFileSync('/path/to/intermediateCertTwo.crt') 
+      ]
+};
+
+let serverHttps = https.createServer(authData, app);
+
+app.setup(serverHttps)
+
+serverHttps.listen(app.get('port'), function () {
+  console.log('Express https server listening on port ', port);
+});
+
+serverHttps.on('listening', () =>
+  logger.info('Feathers application started on https://' + app.get('host') + ':' + port)
 );
